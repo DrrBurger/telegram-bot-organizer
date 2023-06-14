5565896b3e18376cd091876ab94a4c9339b4a03a
dr.burger <mr.krotkov.mm@mail.ru>
2023-06-14 19:59:51 +0400

    Stable but need no count tryes of int

--- main.py
+++ main.py
@@ -222,28 +222,48 @@ async def process_rating_name(message: types.Message, state: FSMContext):
         data['name'] = message.text.lower()
         data['messages_to_delete'].append(message.message_id)
 
+        # Добавляем счетчик попыток
+        if 'attempt_counter' not in data:
+            data['attempt_counter'] = 2
+        else:
+            data['attempt_counter'] -= 1
+
     # проверяем существование места в базе данных
     async with aiosqlite.connect('places.db') as db:
         cursor = await db.cursor()
         await cursor.execute('SELECT * FROM places WHERE name = ?', (data['name'],))
         place = await cursor.fetchone()
         if place is None:
-            sent_message = await message.answer("Такого места не существует в базе данных. Попробуйте ещё раз.")
-            data['messages_to_delete'].append(sent_message.message_id)
-            await asyncio.sleep(2)
-            for msg_id in data['messages_to_delete']:
-                try:
-                    await bot.delete_message(chat_id=message.chat.id, message_id=msg_id)
-                except exceptions.MessageCantBeDeleted:
-                    continue
-            await state.reset_state()
+            async with state.proxy() as data:
+                if data['attempt_counter'] > 0:  # Пользователь может попробовать еще раз
+                    sent_message = await message.answer(f"Такого места не существует в базе данных. Попробуйте ещё раз. Попыток осталось {data['attempt_counter']} ")
+                    data['messages_to_delete'].append(sent_message.message_id)
+                else:  # Пользователь использовал все попытки
+                    sent_message = await message.answer("Такого места не существует в базе данных. Вы исчерпали все попытки.")
+                    data['messages_to_delete'].append(sent_message.message_id)
+
+                    # Удаляем все сообщения
+                    await asyncio.sleep(1)
+                    for msg_id in data['messages_to_delete']:
+                        try:
+                            await bot.delete_message(chat_id=message.chat.id, message_id=msg_id)
+                        except exceptions.MessageCantBeDeleted:
+                            continue
+
+                    data['attempt_counter'] = 3  # Сбрасываем счетчик попыток
+                    await state.reset_state()  # Сбрасываем состояние
+
             return
+        else:
+            # Пользователь ввел допустимое место
+            async with state.proxy() as data:
+                data['attempt_counter'] = 3  # Сбрасываем счетчик попыток
 
-    sent_message = await message.answer("Введите оценку от 1 до 10:")
-    async with state.proxy() as data:
-        data['messages_to_delete'].append(sent_message.message_id)
+            sent_message = await message.answer("Введите оценку от 1 до 10:")
+            async with state.proxy() as data:
+                data['messages_to_delete'].append(sent_message.message_id)
 
-    await Rating.next()
+            await Rating.next()
 
 
 # состояние ввода оценки вызванное предыдущим состоянием
