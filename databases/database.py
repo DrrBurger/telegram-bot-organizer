fc941353cae17b7c19e2bc53792affee40a01b86
dr.burger <mr.krotkov.mm@mail.ru>
2023-06-16 12:00:53 +0400

    db

--- databases/database.py
+++ databases/database.py
@@ -4,27 +4,28 @@ import aiosqlite
 async def create_db():
     async with aiosqlite.connect('places.db') as db:
         cursor = await db.cursor()
-        await cursor.execute('''
+
+        await cursor.executescript('''
             CREATE TABLE IF NOT EXISTS poll_data (
                 poll_id TEXT PRIMARY KEY,
                 options TEXT
-            )
-        ''')
-        await cursor.execute('''
+            );
+
             CREATE TABLE IF NOT EXISTS poll_results (
                 poll_id TEXT,
                 option_id INTEGER
-            )
-        ''')
-        await cursor.execute('''
+            );
+
             CREATE TABLE IF NOT EXISTS places (
                 name text,
                 address text,
-                rating integer DEFAULT 0)
-        ''')
-        await cursor.execute('''
+                rating integer DEFAULT 0
+            );
+
             CREATE TABLE IF NOT EXISTS ratings (
                 name text,
-                rating integer)
+                rating integer
+            );
         ''')
+
         await db.commit()
